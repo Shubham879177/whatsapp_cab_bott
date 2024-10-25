8c9f67a5a4b4ff9444488c63e9d2e86f2842be3e
Shubham879177 <shubhamapps87@gmail.com>
2024-10-25 12:43:29 +0530

    sss

--- bott/views.py
+++ bott/views.py
@@ -1,6 +1,4 @@
-from http.client import HTTPResponse
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HTTPResponse("HII")
+def home(request):
+    return HttpResponse("Welcome to the homepage!")
